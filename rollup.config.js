e099abe531957f562b469092a5a6f82bb8bd1d0b
Will Frankovich <willjf44@gmail.com>
2022-07-29 22:46:56 -0400

    path 1

--- rollup.config.js
+++ rollup.config.js
@@ -15,18 +15,3 @@ export default [
         plugins: [dts()]
     }
 ]
-
-/*
-import dts from "rollup-plugin-dts";
-
-const config = [
-  // …
-  {
-    input: "./my-input/index.d.ts",
-    output: [{ file: "dist/my-library.d.ts", format: "es" }],
-    plugins: [dts()],
-  },
-];
-
-export default config;
-*/
