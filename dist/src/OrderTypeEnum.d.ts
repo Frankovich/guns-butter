d770e1abeca22540099f32cee0b225ef73425794
Will Frankovich <willjf44@gmail.com>
2022-08-04 19:10:37 -0400

    delete dist

--- dist/src/OrderTypeEnum.d.ts
+++
@@ -1,7 +0,0 @@
-export declare enum OrderType {
-    LimitBuy = "limit buy",
-    LimitSell = "limit sell",
-    MarketBuy = "market buy",
-    MarketSell = "market sell",
-    Invalid = "invalid"
-}
--- dist/src/Receipt.d.ts
+++
@@ -1,9 +0,0 @@
-import { ReceiptType } from "./ReceiptTypeEnum";
-export declare class Receipt {
-    outcome: ReceiptType;
-    buyer: string | null;
-    seller: string | null;
-    itemID: string;
-    priceSold: number | null;
-    constructor(outcome: ReceiptType, itemID: string, priceSold: number | null, buyer: string | null, seller: string | null);
-}
